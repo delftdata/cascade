e6f688d9d5bd7dc6e673ac8547397ef1e1f5cb10
MWSchutte <marcus.schutte@live.nl>
2024-12-19 10:47:00 +0100

    Remove old files

--- src/cascade/frontend/intermediate_representation/__init__.py
+++ src/cascade/frontend/intermediate_representation/__init__.py
@@ -1,4 +1,2 @@
-from .block import Block
 from .statement import Statement
 from .statement_level_dataflow_graph import StatementDataflowGraph
-from .dataflow_graph import DataflowGraph
--- src/cascade/frontend/intermediate_representation/edge.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Edge:
-    """ Class that contains edge information.
-    -   variables: The variables flowing over the edges.
-    """
-    pass
