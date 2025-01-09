80766ecc182c73578544ca94d04a140615c49dfb
lucasvanmol <lucas.vanmol@gmail.com>
2025-01-09 16:14:18 +0100

    Mark demo test as integration

--- deathstar/test_demo.py
+++ deathstar/test_demo.py
@@ -1,35 +1,41 @@
 
-from deathstar.demo import DeathStarDemo
+from deathstar.demo import Client, DeathStarDemo
+import time
+import pytest
 
-
+@pytest.mark.integration
 def test_deathstar_demo():
-    ds = DeathStarDemo()
+    ds = DeathStarDemo("deathstardemo-test")
     ds.init_runtime()
     ds.runtime.run(run_async=True)
+    print("Populating, press enter to go to the next step when done")
     ds.populate()
+
+    client = Client("deathstardemo-test")
+    input()
+    print("testing user login")
+    client.user_login()
+
+    input()
+    print("testing reserve")
+    client.reserve()
+
+    input()
+    print("testing search")
+    client.search_hotel()
+
+    input()
+    print("testing recommend (distance)")
+    time.sleep(0.5)
+    client.recommend(req_param="distance")
+
+    input()
+    print("testing recommend (price)")
+    time.sleep(0.5)
+    client.recommend(req_param="price")
+
+    input()
     print("done!")
-
-    input()
-    print("user login")
-    ds.user_login()
-
-    input()
-    print("reserve")
-    ds.reserve()
-
-    input()
-    print("search")
-    ds.search_hotel()
-
-    input()
-    print("recommend")
-    ds.recommend(req_param="distance")
-
-    input()
-    print("recommend")
-    ds.recommend(req_param="price")
-
-    input()
 
 
 if __name__ == "__main__":
